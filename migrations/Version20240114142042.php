ec3fdb66b58e2f55b46fc7cbafddb7e3a31191d6
Nicolas Nielsen <nicolas.nielsen@gmail.com>
2024-01-27 11:58:35 +0100

    fix: fix migrations

--- migrations/Version20240114142042.php
+++ migrations/Version20240114142042.php
@@ -18,11 +18,11 @@ final class Version20240114142042 extends AbstractMigration
     {
         $this->addSql('CREATE TABLE categories (id UUID NOT NULL, parent_category_id UUID DEFAULT NULL, title VARCHAR(100) NOT NULL, description VARCHAR(255) NOT NULL, slug VARCHAR(100) NOT NULL, level INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_64C19C1796A8F92 ON categories (parent_category_id)');
-        $this->addSql('COMMENT ON COLUMN category.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN category.parent_category_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN categories.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN categories.parent_category_id IS \'(DC2Type:uuid)\'');
         $this->addSql('ALTER TABLE categories ADD CONSTRAINT FK_64C19C1796A8F92 FOREIGN KEY (parent_category_id) REFERENCES categories (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE products ADD category_id UUID DEFAULT NULL');
-        $this->addSql('COMMENT ON COLUMN product.category_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN products.category_id IS \'(DC2Type:uuid)\'');
         $this->addSql('ALTER TABLE products ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES categories (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_D34A04AD12469DE2 ON products (category_id)');
     }
